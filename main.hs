9b17acf50dd6d0eefe2e77259796e0c4f15c02f7
Shockk <shokku.ra@gmail.com>
2014-02-12 22:23:49 +0000

    Tidied up repository.

--- main.hs
+++ main.hs
@@ -1,21 +1,19 @@
 import System.Environment (getArgs)
---import System.IO (hSetBuffering, hGetLine, hPutStrLn, hClose, hIsEOF, hIsOpen, BufferMode(..), Handle)
 import System.IO
-import Network (withSocketsDo, listenOn, Socket, PortID(..))
+import Network
 import Network.Socket
+import Network.SocketServer
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.STM
---import Control.Monad (liftM, filterM)
 import Control.Monad
 
+{-
 data Shared = Shared
     { sockets :: [Socket]
     }
 
 data Client = Client (Maybe Socket)
-
 data Env = Env [Client]
-
 data User = User
     { socket :: Maybe Socket
     , id     :: Maybe String
@@ -23,28 +21,19 @@ data User = User
     , user   :: Maybe String
     , real   :: Maybe String
     }
+-}
 
 main :: IO ()
-main = withSocketsDo $ do
-    args <- getArgs
-    sharedT <- atomically $ do newTVar $ Shared []
-    sock <- listenOn $ PortNumber 6667
-    forkIO $ acceptLoop sharedT sock
-    mainLoop sharedT $ Env []
-
-acceptLoop :: TVar Shared -> Socket -> IO ()
-acceptLoop sharedT sock = do
-    (s, _) <- accept sock
-    atomically $ do
-        shared <- readTVar sharedT
-        writeTVar sharedT $ Shared (s : sockets shared)
-    acceptLoop sharedT sock
-
-mainLoop :: TVar Shared -> Env -> IO ()
-mainLoop sharedT env = do
-    shared <- atomically $ do readTVar sharedT
-    putStrLn $ show (sockets shared)
-    --hs <- filterM hIsWritable (handles shared)
-    --putStrLn $ show hs
-    threadDelay 1000000
-    mainLoop sharedT env
+main = do
+    serveTCPforever (simpleTCPOptions 6667) {reuse = True}
+        $ threadedHandler $ handleHandler
+        (\handle _ _ -> do
+            hSetBuffering handle NoBuffering
+            hPutStrLn handle "What's your name?"
+            hPutStr handle "> "
+            hFlush handle
+            line <- hGetLine handle
+            let response = concat ["Hello ", line, "!"]
+            putStrLn response
+            hPutStrLn handle response
+        )
