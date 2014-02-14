84fe22ae138495cd1779fa95d07fde5f58262b75
Shockk <shokku.ra@gmail.com>
2014-02-14 13:52:31 +0000

    Added copyright notice, updated sToMessage

--- main.hs
+++ main.hs
@@ -1,3 +1,18 @@
+{- Copyright 2014 David Farrell <shokku.ra@gmail.com>
+
+ - Licensed under the Apache License, Version 2.0 (the "License");
+ - you may not use this file except in compliance with the License.
+ - You may obtain a copy of the License at
+
+ - http://www.apache.org/licenses/LICENSE-2.0
+
+ - Unless required by applicable law or agreed to in writing, software
+ - distributed under the License is distributed on an "AS IS" BASIS,
+ - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ - See the License for the specific language governing permissions and
+ - limitations under the License.
+ -}
+
 import System.Environment (getArgs)
 import System.IO
 import System.Timeout
@@ -47,7 +62,8 @@ data Client = Client
     }
 
 sToMessage :: String -> Message
-sToMessage (':':s) = Message (Just $ StringPrefix $ head $ words s) "command" []
+sToMessage (':':s) = Message (Just $ StringPrefix $ head $ words s)
+    (words s !! 1) []
 sToMessage s = Message Nothing "command" []
 
 main :: IO ()
