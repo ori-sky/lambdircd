bec152147d1da091a1a507d1c560406921f8c97f
Shockk <shokku.ra@gmail.com>
2014-02-17 12:56:49 +0000

    Wrote Plugin.loadPlugin

--- Plugin.hs
+++ Plugin.hs
@@ -1,6 +1,23 @@
+{- Copyright 2014 David Farrell <shokku.ra@gmail.com>
+
+ - Licensed under the Apache License, Version 2.0 (the "License");
+ - you may not use this file except in compliance with the License.
+ - You may obtain a copy of the License at
+
+ - http://www.apache.org/licenses/LICENSE-2.0
+
+ - Unless required by applicable law or agreed to in writing, software
+ - distributed under the License is distributed on an "AS IS" BASIS,
+ - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ - See the License for the specific language governing permissions and
+ - limitations under the License.
+ -}
+
 {-# LANGUAGE RankNTypes #-}
 
 module Plugin where
+
+import System.Plugins
 
 data Interface = Interface
     { isPlugin  :: Bool
@@ -11,3 +28,10 @@ defaultPlugin = Interface
     { isPlugin  = True
     , name      = ""
     }
+
+loadPlugin :: String -> String -> IO (Maybe Interface)
+loadPlugin includePath path = do
+    p <- pdynload path [includePath] [] "Plugin.Interface" "plugin"
+    case p of
+        LoadSuccess _ a -> return (Just a)
+        LoadFailure e   -> mapM_ putStrLn e >> return Nothing
