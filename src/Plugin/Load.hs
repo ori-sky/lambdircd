2c7815cabc3d40a6d6e9d732b6212895a1e3a1c8
Shockk <shokku.ra@gmail.com>
2014-02-25 00:55:50 +0000

    Plugin.Load: Updated import path

--- src/Plugin/Load.hs
+++ src/Plugin/Load.hs
@@ -20,7 +20,7 @@ import qualified Plugin as Plugin
 
 loadPlugin :: String -> IO (Maybe Plugin.Interface)
 loadPlugin name = do
-    p <- pdynload ("plugins/"++name++".o") ["."] [] "Plugin.Interface" "plugin"
+    p <- pdynload ("plugins/"++name++".o") ["src"] [] "Plugin.Interface" "plugin"
     case p of
         LoadSuccess _ a -> return $ Just $ case Plugin.name a of
             ""  -> a {Plugin.name=name}
