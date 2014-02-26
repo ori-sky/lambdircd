1a1d778245a1d62222d5f16e45a2f32a8ce88be8
Shockk <shokku.ra@gmail.com>
2014-02-26 05:52:11 +0000

    Wrote spec/Client

---
+++ spec/Client.hs
@@ -0,0 +1,15 @@
+import Test.Hspec
+import IRC.Server.Client
+
+main :: IO ()
+main = hspec $ do
+    describe "IRC.Server.Client" $ do
+        it "can be registered" $ do
+            isClientRegistered (Client Nothing Nothing Nothing Nothing) `shouldBe` False
+            isClientRegistered (Client Nothing (Just "nick") Nothing Nothing) `shouldBe` False
+            isClientRegistered (Client Nothing Nothing (Just "user") Nothing) `shouldBe` False
+            isClientRegistered (Client Nothing Nothing Nothing (Just "real")) `shouldBe` False
+            isClientRegistered (Client Nothing (Just "nick") (Just "user") Nothing) `shouldBe` False
+            isClientRegistered (Client Nothing (Just "nick") Nothing (Just "real")) `shouldBe` False
+            isClientRegistered (Client Nothing Nothing (Just "user") (Just "real")) `shouldBe` False
+            isClientRegistered (Client Nothing (Just "nick") (Just "user") (Just "real")) `shouldBe` True
