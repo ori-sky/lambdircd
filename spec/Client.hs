6c55f736e6e873adf2ea18f367c858ade948bd0d
Shockk <shokku.ra@gmail.com>
2014-02-28 23:42:40 +0000

    Fixed Client spec tests

--- spec/Client.hs
+++ spec/Client.hs
@@ -5,11 +5,11 @@ main :: IO ()
 main = hspec $ do
     describe "IRC.Server.Client" $ do
         it "can be registered" $ do
-            isClientRegistered (Client Nothing Nothing Nothing Nothing) `shouldBe` False
-            isClientRegistered (Client Nothing (Just "nick") Nothing Nothing) `shouldBe` False
-            isClientRegistered (Client Nothing Nothing (Just "user") Nothing) `shouldBe` False
-            isClientRegistered (Client Nothing Nothing Nothing (Just "real")) `shouldBe` False
-            isClientRegistered (Client Nothing (Just "nick") (Just "user") Nothing) `shouldBe` False
-            isClientRegistered (Client Nothing (Just "nick") Nothing (Just "real")) `shouldBe` False
-            isClientRegistered (Client Nothing Nothing (Just "user") (Just "real")) `shouldBe` False
-            isClientRegistered (Client Nothing (Just "nick") (Just "user") (Just "real")) `shouldBe` True
+            isClientRegistered (Client Nothing Nothing Nothing Nothing Nothing) `shouldBe` False
+            isClientRegistered (Client Nothing Nothing (Just "nick") Nothing Nothing) `shouldBe` False
+            isClientRegistered (Client Nothing Nothing Nothing (Just "user") Nothing) `shouldBe` False
+            isClientRegistered (Client Nothing Nothing Nothing Nothing (Just "real")) `shouldBe` False
+            isClientRegistered (Client Nothing Nothing (Just "nick") (Just "user") Nothing) `shouldBe` False
+            isClientRegistered (Client Nothing Nothing (Just "nick") Nothing (Just "real")) `shouldBe` False
+            isClientRegistered (Client Nothing Nothing Nothing (Just "user") (Just "real")) `shouldBe` False
+            isClientRegistered (Client Nothing Nothing (Just "nick") (Just "user") (Just "real")) `shouldBe` True
